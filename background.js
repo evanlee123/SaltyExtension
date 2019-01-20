058bd9e9fa2d6a1fe224e46ed14a5a8daf512548
evanlee123 <evanlee123@email.arizona.edu>
2019-01-20 04:12:00 -0700

    added better functionality for option

---
+++ background.js
@@ -0,0 +1,4 @@
+function initialTimer(minutes){
+    var alarm = {"delayInMinutes": parseInt(chrome.storage.sync.get('saltyTimeAllocation')),}
+    //chrome.alarms.create("SaltyInitialTimer")
+}
--- options.js
+++ options.js
@@ -1,14 +1,34 @@
 $(function(){
-    chrome.storage.sync.get('time',function(timer){
-        $('#timeAllocated').text(timer.time)
-    })
+    chrome.storage.sync.get(['saltyTimeAllocated','saltyTimeUnsable'],function(timer){
+        $('#timeAllocated').text(timer.saltyTimeAllocated);
+        $('#timeUnusable').text(timer.saltyTimeUnusable);
+    });
+
+    $('#reset').click(function(){
+        chrome.storage.sync.set({'saltyTimeAllocated': 17, 
+                                 'saltyTimeUnusable': 5});
+        $('#timeUnusable').text(5);
+        $('#timeAllocated').text(17);
+    });
 
     $('#timeAmount').click(function(){
-        var time = parseInt($('#amount').val());
-        chrome.storage.sync.set({'time': time });
-        $('#timeAllocated').text(time);
-        $('#amount').val('');
-    })
+        var timeAllocated = parseInt($('#amount').val());
+        var timeUnusable = parseInt($('#unusableAmount').val());       
+        
+        if (isNaN(timeAllocated)){
+            timeAllocated = parseInt($('#timeAllocated').text());
+        }
+        if (isNaN(timeUnusable)){
+            var timeUnusable = parseInt($('#timeUnusable').text());
+        }
+
+        chrome.storage.sync.set({'saltyTimeAllocated': timeAllocated, 
+                                 'saltyTimeUnusable': timeUnusable});
+
+        $('#timeUnusable').text(timeUnusable);
+        $('#timeAllocated').text(timeAllocated);
+        
+    });
 
 
 });
